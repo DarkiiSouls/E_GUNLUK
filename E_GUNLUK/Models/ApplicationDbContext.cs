ad559e84dcd5582e13c5e0dfaa728a8cb3f6b062
Yusuf YURTSEVER <yusuf.mh@outlook.com>
2017-04-17 23:22:57 +0300

    first init

---
+++ E_GUNLUK/Models/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace E_GUNLUK.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Note> notes { get; set; }
+        public DbSet<Tags> tags { get; set; }
+        public DbSet<Comments> comments { get; set; }
+        public DbSet<Likes> likes { get; set; }
+        public DbSet<FriendsList> friendsList { get; set; }
+        public DbSet<Favorites> favorites { get; set; }
+
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
---
+++ E_GUNLUK/Models/Note.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using System.Linq;
+using System.Web;
+
+namespace E_GUNLUK.Models
+{
+    public class Note
+    {
+        public  ApplicationUser NoteTaker { get; set; }
+        [Key]
+        [Column(Order = 1)]
+        public int NoteId { get; set; }
+        public string Title { get; set; }
+        [Required]
+        public string NoteText { get; set; }
+        [Required]
+        public DateTime NoteDate { get; set; }
+        [Required]
+        public bool PubOrPvt { get; set; }
+
+    }
+}
